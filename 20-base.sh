c49e98620db2a424f34b9c68acbf8e1a0a8840fd
c3n21 <me@zhifan.me>
2022-03-13 14:36:02 +0100

    Added base

---
+++ 20-base.sh
@@ -0,0 +1,39 @@
+AddPackage --foreign memtest86-efi # A free, thorough, stand alone memory test as an EFI application
+AddPackage --foreign surfshark-vpn # Surfshark VPN client
+AddPackage --foreign pacman-cleanup-hook # hook to cleanup pacman cache keeping only the installed plus next most recent packages
+AddPackage --foreign paru # Feature packed AUR helper
+AddPackage dbus-broker # Linux D-Bus Message Broker
+AddPackage --foreign neovim-nightly-bin # Fork of Vim aiming to improve user experience, plugins, and GUIs - Nightly Builds
+AddPackage --foreign nohang-git # A sophisticated low memory handler
+AddPackage edk2-ovmf # Firmware for Virtual Machines (x86_64, i686)
+AddPackage efibootmgr # Linux user-space application to modify the EFI Boot Manager
+AddPackage firejail # Linux namespaces sandbox program
+AddPackage haveged # Entropy harvesting daemon using CPU timings
+AddPackage fish # Smart and user friendly shell intended mostly for interactive use
+AddPackage btrfs-progs # Btrfs filesystem utilities
+AddPackage acpid # A daemon for delivering ACPI power management events with netlink support
+AddPackage base # Minimal package set to define a basic Arch Linux installation
+AddPackage bat # Cat clone with syntax highlighting and git integration
+AddPackage man-db # A utility for reading man pages
+AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
+AddPackage ripgrep-all # rga: ripgrep, but also search in PDFs, E-Books, Office documents, zip, tar.gz, etc.
+AddPackage rsync # A fast and versatile file copying tool for remote and local files
+AddPackage sudo # Give certain users the ability to run some commands as root
+AddPackage unrar # The RAR uncompression program
+AddPackage which # A utility to show the full path of commands
+AddPackage htop # Interactive process viewer
+AddPackage linux-firmware # Firmware files for Linux
+AddPackage networkmanager # Network connection manager and user applications
+AddPackage ntfs-3g # NTFS filesystem driver and utilities
+AddPackage linux-zen # The Linux ZEN kernel and modules
+AddPackage linux-zen-headers # Headers and scripts for building modules for the Linux ZEN kernel
+AddPackage lshw # A small tool to provide detailed information on the hardware configuration of the machine.
+AddPackage refind # An EFI boot manager
+AddPackage snap-pac # Pacman hooks that use snapper to create pre/post btrfs snapshots like openSUSE's YaST
+AddPackage snapper # A tool for managing BTRFS and LVM snapshots. It can create, diff and restore snapshots and provides timelined auto-snapping.
+AddPackage sshfs # FUSE client based on the SSH File Transfer Protocol
+AddPackage samba # SMB Fileserver and AD Domain server
+AddPackage smbnetfs # small C program that mounts Microsoft network neighborhood in single directory.
+AddPackage intel-ucode # Microcode update files for Intel CPUs
+AddPackage yt-dlp # A youtube-dl fork with additional features and fixes
+AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
