fa46a8cacd86d321dc99d5e138dd2d31e7fd14f4
c3n21 <me@zhifan.me>
2022-03-14 10:36:16 +0100

    add: gnome-calendar, smb support

--- 40-gui.sh
+++ 40-gui.sh
@@ -3,11 +3,13 @@ AddPackage --foreign gnome-shell-extension-pop-shell-git # Pop Shell - Tiling wi
 AddPackage --foreign gnome-shell-extension-sound-output-device-chooser-git # Enable selection of sound source and sink devices.
 AddPackage baobab
 AddPackage gdm
+AddPackage gnome-calendar # Simple and beautiful calendar application designed to perfectly fit the GNOME desktop
 AddPackage gnome-control-center # GNOME's main interface to configure various aspects of the desktop
 AddPackage gnome-keyring # Stores passwords and encryption keys
 AddPackage gnome-settings-daemon
 AddPackage gnome-shell
 AddPackage gnome-themes-extra # Extra Themes for GNOME Applications
 AddPackage gnome-tweaks # Graphical interface for advanced GNOME 3 settings (Tweak Tool)
+AddPackage gvfs-smb # Virtual filesystem implementation for GIO (SMB/CIFS backend; Windows client)
 AddPackage nautilus
 AddPackage seahorse # GNOME application for managing PGP keys.
