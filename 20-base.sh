e95b55efea4e1990d71e33a01571446f445fa5ea
c3n21 <me@zhifan.me>
2022-03-19 10:28:05 +0100

    update

--- 20-base.sh
+++ 20-base.sh
@@ -1,39 +1,46 @@
+AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
 AddPackage --foreign memtest86-efi # A free, thorough, stand alone memory test as an EFI application
-AddPackage --foreign surfshark-vpn # Surfshark VPN client
-AddPackage --foreign pacman-cleanup-hook # hook to cleanup pacman cache keeping only the installed plus next most recent packages
-AddPackage --foreign paru # Feature packed AUR helper
-AddPackage dbus-broker # Linux D-Bus Message Broker
 AddPackage --foreign neovim-nightly-bin # Fork of Vim aiming to improve user experience, plugins, and GUIs - Nightly Builds
 AddPackage --foreign nohang-git # A sophisticated low memory handler
+AddPackage --foreign pacman-cleanup-hook # hook to cleanup pacman cache keeping only the installed plus next most recent packages
+AddPackage --foreign paru # Feature packed AUR helper
+AddPackage --foreign surfshark-vpn # Surfshark VPN client
+AddPackage acpid # A daemon for delivering ACPI power management events with netlink support
+AddPackage autoconf # A GNU tool for automatically configuring source code
+AddPackage automake # A GNU tool for automatically creating Makefiles
+AddPackage base # Minimal package set to define a basic Arch Linux installation
+AddPackage bat # Cat clone with syntax highlighting and git integration
+AddPackage bison # The GNU general-purpose parser generator
+AddPackage btrfs-progs # Btrfs filesystem utilities
+AddPackage dbus-broker # Linux D-Bus Message Broker
 AddPackage edk2-ovmf # Firmware for Virtual Machines (x86_64, i686)
 AddPackage efibootmgr # Linux user-space application to modify the EFI Boot Manager
 AddPackage firejail # Linux namespaces sandbox program
-AddPackage haveged # Entropy harvesting daemon using CPU timings
 AddPackage fish # Smart and user friendly shell intended mostly for interactive use
-AddPackage btrfs-progs # Btrfs filesystem utilities
-AddPackage acpid # A daemon for delivering ACPI power management events with netlink support
-AddPackage base # Minimal package set to define a basic Arch Linux installation
-AddPackage bat # Cat clone with syntax highlighting and git integration
-AddPackage man-db # A utility for reading man pages
-AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
-AddPackage ripgrep-all # rga: ripgrep, but also search in PDFs, E-Books, Office documents, zip, tar.gz, etc.
-AddPackage rsync # A fast and versatile file copying tool for remote and local files
-AddPackage sudo # Give certain users the ability to run some commands as root
-AddPackage unrar # The RAR uncompression program
-AddPackage which # A utility to show the full path of commands
+AddPackage flex # A tool for generating text-scanning programs
+AddPackage haveged # Entropy harvesting daemon using CPU timings
 AddPackage htop # Interactive process viewer
+AddPackage intel-ucode # Microcode update files for Intel CPUs
 AddPackage linux-firmware # Firmware files for Linux
-AddPackage networkmanager # Network connection manager and user applications
-AddPackage ntfs-3g # NTFS filesystem driver and utilities
 AddPackage linux-zen # The Linux ZEN kernel and modules
 AddPackage linux-zen-headers # Headers and scripts for building modules for the Linux ZEN kernel
 AddPackage lshw # A small tool to provide detailed information on the hardware configuration of the machine.
+AddPackage m4 # The GNU macro processor
+AddPackage man-db # A utility for reading man pages
+AddPackage networkmanager # Network connection manager and user applications
+AddPackage ntfs-3g # NTFS filesystem driver and utilities
+AddPackage pkgconf # Package compiler and linker metadata toolkit
 AddPackage refind # An EFI boot manager
+AddPackage reflector # A Python 3 module and script to retrieve and filter the latest Pacman mirror list.
+AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
+AddPackage ripgrep-all # rga: ripgrep, but also search in PDFs, E-Books, Office documents, zip, tar.gz, etc.
+AddPackage rsync # A fast and versatile file copying tool for remote and local files
+AddPackage samba # SMB Fileserver and AD Domain server
+AddPackage smbnetfs # small C program that mounts Microsoft network neighborhood in single directory.
 AddPackage snap-pac # Pacman hooks that use snapper to create pre/post btrfs snapshots like openSUSE's YaST
 AddPackage snapper # A tool for managing BTRFS and LVM snapshots. It can create, diff and restore snapshots and provides timelined auto-snapping.
 AddPackage sshfs # FUSE client based on the SSH File Transfer Protocol
-AddPackage samba # SMB Fileserver and AD Domain server
-AddPackage smbnetfs # small C program that mounts Microsoft network neighborhood in single directory.
-AddPackage intel-ucode # Microcode update files for Intel CPUs
+AddPackage sudo # Give certain users the ability to run some commands as root
+AddPackage unrar # The RAR uncompression program
+AddPackage which # A utility to show the full path of commands
 AddPackage yt-dlp # A youtube-dl fork with additional features and fixes
-AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
